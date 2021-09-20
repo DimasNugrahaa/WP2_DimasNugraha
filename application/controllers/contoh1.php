52b2e696380e81d5ce3ae9ab807131622c95f797
DimasNugrahaa <dimasnugraha0504@gmail.com>
2021-09-20 18:48:19 +0700

    upload file project latihan

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Dimas Nugraha dengan NIM : 12200400 Kelas : 12.3A.13.
+ Saya tinggal di daerah Bondongan.
+ Olahraga yang saya sukai adalah
+ Bulutangkis dan bermain layangan.";
+ }
+}
