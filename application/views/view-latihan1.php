52b21fb466968e9e85851b47606e74394e3729f1
DimasNugrahaa <dimasnugraha0504@gmail.com>
2021-09-29 23:50:15 +0700

    Upload Tugas Pertemuan2

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <tittle></tittle>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methone penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
