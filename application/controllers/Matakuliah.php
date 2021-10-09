13aabbda5e4cc2b784769a6e62921efafc2be3c3
DimasNugrahaa <dimasnugraha0504@gmail.com>
2021-10-10 01:24:57 +0700

    Upload Tugas Pertemuan 4

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,33 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+
+    public function index()
+
+    {
+        $this->load->view('view-from-matakuliah');
+    }
+    public function cetak()
+    {
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 
+        'required|min_length[3]', [
+        'required' => 'Kode Matakuliah Harus diisi',
+        'min_lenght' => 'Kode terlalu pendek'
+    ]);
+    $this->form_validation->set_rules('nama', 'Nama Matakuliah', 
+        'required|min_length[3]', [
+        'required' => 'Nama Matakuliah Harus diisi',
+        'min_lenght' => 'Nama terlalu pendek'
+    ]);
+    if ($this->form_validation->run() != true) {
+        $this->load->view('view-from-matakuliah');
+    } else {
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks')
+        ];
+        $this->load->view('view-data-matakuliah', $data);
+    }
+}
+}
