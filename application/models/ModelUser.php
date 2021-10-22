27bd05cbee801a4ac462a1b813c9dc1faffe17a3
DimasNugrahaa <dimasnugraha0504@gmail.com>
2021-10-23 02:45:20 +0700

    Upload Tugas Pertemuan 5

---
+++ application/models/ModelUser.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class ModelUser extends CI_Model
+{
+    public function simpanData($data = null)
+    {$this->db->insert('user' , $data);}
+
+    public function cekData($where = null)
+    {return $this->db->get_where('user' , $where);}
+
+    public function getUserWhere($where = null)
+    {return $this->db->get_where('user' , $where);}
+
+    public function getUserLimit()
+    {
+    $this->db->select('*' );
+    $this->db->from('user' );
+    $this->db->limit(10, 0);
+    return $this->db->get();
+    }
+}
