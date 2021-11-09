25abde67352734db09c0f5d4c70cbc341f600a49
DimasNugrahaa <dimasnugraha0504@gmail.com>
2021-11-10 03:07:16 +0700

    Upload Tugas Pertemuan 7

---
+++ application/controllers/Admin.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+    }
+    public function index()
+    {
+        $data['judul'] = 'Dashboard';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['anggota'] = $this->ModelUser->getUserLimit()->result_array();
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+        $this->load->view('templates/Header', $data);
+        $this->load->view('templates/Sidebar', $data);
+        $this->load->view('templates/Topbar', $data);
+        $this->load->view('admin/index', $data);
+        $this->load->view('templates/Footer');
+    }
+}
