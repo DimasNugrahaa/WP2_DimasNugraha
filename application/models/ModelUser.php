fa5a4c61be46c8aebe2b090820e0f749daf48913
DimasNugrahaa <dimasnugraha0504@gmail.com>
2021-12-06 13:43:40 +0700

    Revisi Tugas Pertemuan 3 (Kurang V_about.php)

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -2,20 +2,34 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class ModelUser extends CI_Model
 {
+    public function getUser()
+    {
+        return $this->db->get('user');
+    }
     public function simpanData($data = null)
-    {$this->db->insert('user' , $data);}
-
+    {
+        $this->db->insert('user', $data);
+    }
     public function cekData($where = null)
-    {return $this->db->get_where('user' , $where);}
-
+    {
+        return $this->db->get_where('user', $where);
+    }
     public function getUserWhere($where = null)
-    {return $this->db->get_where('user' , $where);}
-
+    {
+        return $this->db->get_where('user', $where);
+    }
+    public function cekUserAccess($where = null)
+    {
+        $this->db->select('*');
+        $this->db->from('access_menu');
+        $this->db->where($where);
+        return $this->db->get();
+    }
     public function getUserLimit()
     {
-    $this->db->select('*' );
-    $this->db->from('user' );
-    $this->db->limit(10, 0);
-    return $this->db->get();
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->limit(10, 0);
+        return $this->db->get();
     }
 }
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dimas</li>
+        <li>Nama Belakang : Nugraha</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Bondongan Jerokutakaum No.18</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Palembang</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Futsal</li>
+    </ul>
+</section>
